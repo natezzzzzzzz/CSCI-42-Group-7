e1075bee6d97828766574d88444c50b36eda70c0
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2026-04-18 20:22:06 +0800

    Build Update

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
+cd neurodeck
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# This creates the superuser using environment variables
+if [ "$CREATE_SUPERUSER" ]; then
+  python manage.py shell << END
