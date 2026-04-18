cef24083999957044f8dfaecc4e070808bc817ed
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2026-04-18 20:32:29 +0800

    Build Fix 2

--- build.sh
+++ build.sh
@@ -4,26 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 
 cd neurodeck
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 
 # create superuser
-if [ "$CREATE_SUPERUSER" ]; then
-  python manage.py shell << END
-
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-# Move into the directory where manage.py lives
-cd neurodeck
-
-python manage.py collectstatic --noinput
-python manage.py migrate
-
-# This creates the superuser using environment variables
 if [ "$CREATE_SUPERUSER" ]; then
   python manage.py shell << END
 from django.contrib.auth import get_user_model
@@ -36,11 +21,8 @@ password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
-    print(f"Superuser '{username}' created successfully.")
+    print(f"Superuser created successfully.")
 else:
-    print(f"Superuser '{username}' already exists. Skipping creation.")
+    print(f"Superuser already exists. Skipping.")
 END
 fi
-
-
-
